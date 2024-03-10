00877071f6cef4e3573b0a04e6a6a8f2f711a393
Imran Bin Azhar <imran.azhar@drest.com>
2024-03-10 00:44:43 +0000

    Update-Battle instantiator
    Updated battle instantiator to act as a main brain of the game which wil iterate all of the units in all armies

--- DCL Battle Exercise/Assets/Exercise/Battle/Scripts/Army.cs
+++ DCL Battle Exercise/Assets/Exercise/Battle/Scripts/Army.cs
@@ -3,25 +3,75 @@ using UnityEngine;
 
 public class Army
 {
+    //public class UnitsByType
+    //{
+    //    public UnitType unitType { get; private set; }
+    //    public UnitBase[] units;
+
+    //    public UnitsByType(UnitType unitType, int unitsCount)
+    //    {
+    //        this.unitType = unitType;
+    //        units = new UnitBase[unitsCount];
+    //    }
+    //}
+
+    private ArmyModelSO _armyModel;
+
     public Army enemyArmy;
-    public Color color;
-    public List<Warrior> warriors = new List<Warrior>();
-    public List<Archer> archers = new List<Archer>();
+    public string name => _armyModel.name;
+    public Color color => _armyModel.color;
+    public ArmyStrategy armyStrategy => _armyModel.strategy;
 
-    public List<GameObject> GetUnits()
+    //public UnitsByType[] unitsByTypes;
+
+    public List<UnitBase> _allUnits { get; private set; }
+
+    public Army(ArmyModelSO armyModel, UnitsDatabaseSO unitsDatabase, Bounds bounds, Transform parent)
     {
-        List<GameObject> result = new List<GameObject>();
+        UnitModelSO unitModel;
+        GameObject unitObject;
+        _armyModel = armyModel;
+        _allUnits = new List<UnitBase>();
+        UnitBase unitBase = null;
 
-        foreach ( var warrior in warriors )
+        for (int i = 0; i < _armyModel.armyData.Length; i++)
         {
-            result.Add( warrior.gameObject );
+            unitModel = unitsDatabase.GetModelByType(_armyModel.armyData[i].unitType);
+            for (int j = 0; j < _armyModel.armyData[i].amount; j++)
+            {
+                unitObject = Object.Instantiate(unitModel.prefab, parent);
+                switch (unitModel.unitType)
+                {
+                    case UnitType.ARCHER:
+                        unitBase = new Archer(unitObject, this, armyModel, bounds);
+                        break;
+                    case UnitType.WARRIOR:
+                    default:
+                        unitBase = new Warrior(unitObject, this, armyModel, bounds);
+                        break;
+                }
+                _allUnits.Add(unitBase);
+            }
         }
 
-        foreach ( var archer in archers )
-        {
-            result.Add( archer.gameObject );
-        }
+    }
 
-        return result;
+    public List<UnitBase> GetUnits()
+    {
+        return _allUnits;
+    }
+
+    public void UpdateArmy()
+    {
+        for (int i = 0; i < _allUnits.Count; i++)
+        {
+            _allUnits[i].Update();
+        }
+    }
+
+    public void RemoveUnit(UnitBase unitBase)
+    {
+        _allUnits.Remove(unitBase);
+        Object.Destroy(unitBase.gameObject);
     }
 }
--- DCL Battle Exercise/Assets/Exercise/Battle/Scripts/BattleInstantiator.cs
+++ DCL Battle Exercise/Assets/Exercise/Battle/Scripts/BattleInstantiator.cs
@@ -7,88 +7,106 @@ public class BattleInstantiator : MonoBehaviour
 {
     public static BattleInstantiator instance { get; private set; }
 
-    [SerializeField]
-    private ArmyModelSO army1Model;
+    [SerializeField] private ArmyDatabaseSO _armyDatabase;
+    [SerializeField] private UnitsDatabaseSO _unitsDatabase;
+    [SerializeField] private PrefabDatabaseSO _prefabsDatabase;
+    [SerializeField] private BoxCollider[] _spawnBounds;
+    [SerializeField] private GameOverMenu gameOverMenu;
 
-    [SerializeField]
-    private ArmyModelSO army2Model;
-
-    [SerializeField]
-    private Warrior warriorPrefab;
-
-    [SerializeField]
-    private Archer archerPrefab;
-
-    [SerializeField]
-    private BoxCollider leftArmySpawnBounds;
-
-    [SerializeField]
-    private BoxCollider rightArmySpawnBounds;
-
-    public readonly Army army1 = new Army();
-    public readonly Army army2 = new Army();
-
-    public Color army1Color;
-    public Color army2Color;
-
-    public GameOverMenu gameOverMenu;
-
-    void InstanceArmy(IArmyModel model, Army army, Bounds instanceBounds)
-    {
-        for ( int i = 0; i < model.warriors; i++ )
-        {
-            GameObject go = Instantiate(warriorPrefab.gameObject);
-            go.transform.position = Utils.GetRandomPosInBounds(instanceBounds);
-
-            go.GetComponentInChildren<Warrior>().army = army;
-            go.GetComponentInChildren<Warrior>().armyModel = model;
-            go.GetComponentInChildren<Renderer>().material.color = army.color;
-
-            army.warriors.Add(go.GetComponent<Warrior>());
-        }
-
-        for ( int i = 0; i < model.archers; i++ )
-        {
-            GameObject go = Object.Instantiate(archerPrefab.gameObject);
-            go.transform.position = Utils.GetRandomPosInBounds(instanceBounds);
-
-            go.GetComponentInChildren<Archer>().army = army;
-            go.GetComponentInChildren<Archer>().armyModel = model;
-            go.GetComponentInChildren<Renderer>().material.color = army.color;
-
-            army.archers.Add(go.GetComponent<Archer>());
-        }
-    }
+    public PrefabDatabaseSO prefabDatabase => _prefabsDatabase;
+    private Army[] _armies;
+    private Vector3 _forwardTarget;
 
     void Awake()
     {
         instance = this;
 
-        army1.color = army1Color;
-        army1.enemyArmy = army2;
+        Initialise();
+        AssignEnemies();
+    }
 
-        army2.color = army2Color;
-        army2.enemyArmy = army1;
+    private void Initialise()
+    {
+        _armies = new Army[_armyDatabase.armyModelSOs.Count];
+        for (int i = 0; i < _armyDatabase.armyModelSOs.Count; i++)
+        {
+            _armies[i] = new Army(_armyDatabase.armyModelSOs[i], _unitsDatabase,
+                _spawnBounds[i].bounds, new GameObject(_armyDatabase.armyModelSOs[i].armyTitle).transform);
+        }
+    }
 
-        InstanceArmy(army1Model, army1, leftArmySpawnBounds.bounds);
-        InstanceArmy(army2Model, army2, rightArmySpawnBounds.bounds);
+    private void AssignEnemies()
+    {
+        for (int i = 0; i < _armies.Length; i++)
+        {
+            for (int j = 0; j < _armies.Length; j++)
+            {
+                if(i != j)
+                {
+                    _armies[i].enemyArmy = _armies[j];
+                    break;
+                }
+            }
+        }
+    }
+
+    public Army GetWonArmy()
+    {
+        for (int i = 0; i < _armies.Length; i++)
+        {
+            if (_armies[i].GetUnits().Count > 0)
+                return _armies[i];
+        }
+
+        return null;
+    }
+
+    private bool HasAnyArmyWon()
+    {
+        int count = _armies.Length;
+        for (int i = 0; i < _armies.Length; i++)
+        {
+            if (_armies[i].GetUnits().Count == 0)
+                count--;
+        }
+
+        return count == 1;
+    }
+
+    private Vector3 GetArmiesCenter()
+    {
+        Vector3 center = new Vector3();
+        for (int i = 0; i < _armies.Length; i++)
+        {
+            center += Utils.GetCenter(_armies[i].GetUnits());
+        }
+
+        return center;
     }
 
     void Update()
     {
-        if ( army1.GetUnits().Count == 0 || army2.GetUnits().Count == 0 )
+        if (HasAnyArmyWon())
         {
             gameOverMenu.gameObject.SetActive(true);
             gameOverMenu.Populate();
         }
+        else
+        {
+            //update all units
+            for (int i = 0; i < _armies.Length; i++)
+            {
+                _armies[i].UpdateArmy();
+            }
+        }
 
-        Vector3 mainCenter = Utils.GetCenter(army1.GetUnits()) + Utils.GetCenter(army2.GetUnits());
+        Vector3 mainCenter = GetArmiesCenter();
         mainCenter *= 0.5f;
 
-        forwardTarget = (mainCenter - Camera.main.transform.position).normalized;
+        _forwardTarget = (mainCenter - Camera.main.transform.position).normalized;
 
-        Camera.main.transform.forward += (forwardTarget - Camera.main.transform.forward) * 0.1f;
+        Camera.main.transform.forward += (_forwardTarget - Camera.main.transform.forward) * 0.1f;
     }
 
-    private Vector3 forwardTarget;
+    
 }
